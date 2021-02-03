658592daa25176bf96c13955e7cecefdb6823e30
lena_11-81@mail.ru <rehcs2020>
2021-02-03 14:41:01 +0300

    test

---
+++ 2_dz5.js
@@ -0,0 +1,25 @@
+function Korzina (sum, kol, promoKod = null) {
+    if (promoKod === 'ДАРИМ300') {
+        if (sum > 300) {
+            sum = sum - 300;
+        } else {
+            sum = 0;
+        }
+    }
+    if (kol >= 10)
+        sum = sum * 0.95;
+
+    if (sum > 50000) {
+        sum = sum - 50000;
+        sum = sum * 0.8;
+        sum = sum + 50000;
+    }
+    if (promoKod === 'СКИДКА15' && sum >= 20000) {
+        sum = sum * 0.85
+    }
+
+    console.log(sum);
+}
+Korzina(60000, 5, 'ДАРИМ300');
+Korzina(60000, 11, 'СКИДКА15');
+Korzina(500, 1);
